c0d600b01f026a14f326eb511e0707c8a5d07750
srad25 <suzanne.radlein@gmail.com>
2021-04-07 00:33:53 -0400

    added routing and server js

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const fs = require("fs");
+
+
+// Sets up the Express App
+
+var app = express();
+var PORT = process.env.PORT || 8080
+
+// Sets up the Express app to handle data parsing
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/assets", express.static("./assets"));
+
+
+require("./routes/htmlRoute")(app);
+require("./routes/apiRoute")(app);
+
+// Starts the server to begin listening
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
